d626f45b6acc8e1fee0dbcdfebb64e082d25c3d1
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-11-27 21:57:44 +0200

    basic structure

---
+++ flavorfleet/src/main.jsx
@@ -0,0 +1,25 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+import "./index.css";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="menu" element={<Menu />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
