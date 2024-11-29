212d2150b1b5c92e81b75ab6c89a9ea4f01c0cf8
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-11-29 17:55:41 +0200

    res footer and loading elements

---
+++ flavorfleet/src/helpers/Footer.jsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <footer className="bg-zinc-50 text-center dark:bg-neutral-700 lg:text-left mt-4">
+      <div className="bg-black/5 p-4 text-center text-surface dark:text-white">
+        © 2023 Copyright:
+        <a
+          href="https://mohamed-ibrahim-omega-two-17.vercel.app/"
+          className="font-semibold text-blue-600"
+        >
+          {" "}
+          Mohamed Ibrahim
+        </a>
+      </div>
+    </footer>
+  );
+}
