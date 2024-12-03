daad0480b729d0f76f2ad9b6d00e51624a2fc4b7
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-12-03 23:59:19 +0200

    Update payment.js

--- flavorfleet/src/stripe/payment.js
+++ flavorfleet/src/stripe/payment.js
@@ -3,7 +3,9 @@ import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
 
 // Load your Stripe publishable key
-const stripePromise = loadStripe("your-publishable-key-here");
+const stripePromise = loadStripe(
+  "pk_live_51QRhM5ArW04A4HUBVmpAH8hJnOesiwSz3f7Z2wPmhENtfccH2Fz1cv0A6AG4AG0kFt21R02eKFizfZNstmwpIUzX00ur6OQHwh"
+);
 
 const PaymentPage = () => {
   return (
