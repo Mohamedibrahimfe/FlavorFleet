ff485e7e6d8928c809594d59331b1a50b6c80d3c
MOHAMED IBRAHIM <147580141+Mohamedibrahimfe@users.noreply.github.com>
2025-02-19 00:45:13 +0200

    all works hamdullah

--- flavorfleet/src/components/SearchComponent.jsx
+++ flavorfleet/src/components/SearchComponent.jsx
@@ -10,7 +10,7 @@ export default function SearchComponent() {
     <div>
       <input
         type="search"
-        className=" w-full lg:w-72  lg:mx-4 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+        className=" w-full lg:w-64 lg:mr-28 md:hidden lg:block  rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
         placeholder="Search"
         onChange={handleChange}
         onFocus={scrollDown}
